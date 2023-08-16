cfc9dbc94fe34f88f3e179afe4707dea03aaf4fa
Nith21 <vknitheesh01@gmail.com>
2023-08-16 10:30:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const date = new Date();
+  const days = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
+  // console.log(date.getTimezoneOffset())
+  const [currentDate, setCurrentDate] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  useEffect(() => {
+    setCurrentDate({
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds(),
+    });
+  });
+
+  function Hour(h) {
+    if (h >= 12) {
+      return h - 12;
+    }
+    return h;
+  }
+
+  const amPm = currentDate.hours >= 12 ? "PM" : "AM";
+
+  function Zero(h) {
+    const length = h.toString().length;
+    if (length <= 1) {
+      return "0" + h;
+    }
+    return h;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="head">
+          <h1>Digital - Clock</h1>
+          <h1 className="day">
+            {" "}
+            <strong> {days[date.getDay()]} </strong>{" "}
+          </h1>
+        </div>
+        <div className="timeContainer">
+          <div className="time">{Hour(Zero(currentDate.hours))}</div>
+          <span className="firstSpan">:</span>
+          <div className="time">{Zero(currentDate.minutes)}</div>
+          <span className="secondSpan">:</span>
+          <div className="time seconds">{Zero(currentDate.seconds)}</div>
+          <span className="thirdSpan">:</span>
+          <span className="AMPM">{amPm}</span>
+        </div>
+      </div>
+      {/* <div className="dayDiv">
+        <span className="day">{days[date.getDay()]}</span>
+      </div> */}
+    </>
   );
 }
 
